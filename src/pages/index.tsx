169284c9efb9cecf887050ce60354b5fb9144a71
roquiles <roberta_btg@hotmail.com>
2022-06-30 09:32:26 -0300

    Create fetch images feature

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,12 +3,33 @@ import { useMemo } from 'react';
 import { useInfiniteQuery } from 'react-query';
 
 import { Header } from '../components/Header';
-import { CardList } from '../components/CardList';
+import { Card, CardList } from '../components/CardList';
 import { api } from '../services/api';
 import { Loading } from '../components/Loading';
 import { Error } from '../components/Error';
 
+interface DataType {
+  after: string;
+  data: Card[];
+}
+
 export default function Home(): JSX.Element {
+  const fetchImages = async ({ pageParam = null }): Promise<DataType> => {
+    if (pageParam) {
+      const { data } = await api.get('/api/images', {
+        params: {
+          after: pageParam,
+        },
+      });
+
+      return data;
+    }
+
+    const { data } = await api.get('/api/images');
+
+    return data;
+  };
+
   const {
     data,
     isLoading,
@@ -19,17 +40,36 @@ export default function Home(): JSX.Element {
   } = useInfiniteQuery(
     'images',
     // TODO AXIOS REQUEST WITH PARAM
-    ,
+    fetchImages,
     // TODO GET AND RETURN NEXT PAGE PARAM
+    {
+      getNextPageParam: lastPage => lastPage.after ?? null,
+    }
   );
 
   const formattedData = useMemo(() => {
     // TODO FORMAT AND FLAT DATA ARRAY
+    let formattedDataArray = [];
+    const dataPages = data?.pages;
+
+    dataPages?.map(page => {
+      formattedDataArray = [...formattedDataArray, ...page.data];
+      // eslint-disable-next-line no-useless-return
+      return;
+    });
+
+    return formattedDataArray;
   }, [data]);
 
   // TODO RENDER LOADING SCREEN
+  if (isLoading) {
+    return <Loading />;
+  }
 
   // TODO RENDER ERROR SCREEN
+  if (isError) {
+    return <Error />;
+  }
 
   return (
     <>
@@ -37,7 +77,13 @@ export default function Home(): JSX.Element {
 
       <Box maxW={1120} px={20} mx="auto" my={20}>
         <CardList cards={formattedData} />
-        {/* TODO RENDER LOAD MORE BUTTON IF DATA HAS NEXT PAGE */}
+        {hasNextPage && (
+          <Button
+            onClick={() => fetchNextPage({ pageParam: data.pages[0]?.after })}
+          >
+            {isFetchingNextPage ? 'Carregando...' : 'Carregar mais'}
+          </Button>
+        )}
       </Box>
     </>
   );
