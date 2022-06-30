c5093505c18f5a0a4794735b38c5020003b6de7d
roquiles <roberta_btg@hotmail.com>
2022-06-30 18:34:17 -0300

    Create view image modal window

--- src/components/Modal/ViewImage.tsx
+++ src/components/Modal/ViewImage.tsx
@@ -19,5 +19,28 @@ export function ModalViewImage({
   onClose,
   imgUrl,
 }: ModalViewImageProps): JSX.Element {
-  // TODO MODAL WITH IMAGE AND EXTERNAL LINK
+  return (
+    <Modal isOpen={isOpen} onClose={onClose}>
+      <ModalOverlay />
+      <ModalContent maxW="900px" maxH="600px" w="fit-content" h="fit-content">
+        <ModalBody padding={0}>
+          <Image
+            src={imgUrl}
+            alt="Selected image"
+            maxH="600px"
+            maxW="900px"
+            objectFit="cover"
+          />
+        </ModalBody>
+        <ModalFooter
+          h="8"
+          bgColor="pGray.800"
+          justifyContent="left"
+          borderRadius="0 0 6px 6px"
+        >
+          <Link href={imgUrl}>Abrir original</Link>
+        </ModalFooter>
+      </ModalContent>
+    </Modal>
+  );
 }
