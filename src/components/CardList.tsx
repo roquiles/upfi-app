2a7b38d956f06da2d10eba43b4fea8e51cd9b6b9
roquiles <roberta_btg@hotmail.com>
2022-06-30 18:32:45 -0300

    Create image view from modal window

--- src/components/CardList.tsx
+++ src/components/CardList.tsx
@@ -3,7 +3,7 @@ import { useState } from 'react';
 import { Card } from './Card';
 import { ModalViewImage } from './Modal/ViewImage';
 
-interface Card {
+export interface Card {
   title: string;
   description: string;
   url: string;
@@ -16,17 +16,32 @@ interface CardsProps {
 }
 
 export function CardList({ cards }: CardsProps): JSX.Element {
-  // TODO MODAL USEDISCLOSURE
+  const { isOpen, onClose, onOpen } = useDisclosure();
 
-  // TODO SELECTED IMAGE URL STATE
+  const [selectedImageURL, setSelectedImageURL] = useState('');
 
-  // TODO FUNCTION HANDLE VIEW IMAGE
+  function handleViewImage(url: string): void {
+    setSelectedImageURL(url);
+    onOpen();
+  }
 
   return (
     <>
-      {/* TODO CARD GRID */}
+      <SimpleGrid columns={3} spacing={10} mb="40px">
+        {cards.map(cardItem => (
+          <Card
+            key={cardItem.id}
+            data={cardItem}
+            viewImage={() => handleViewImage(cardItem.url)}
+          />
+        ))}
+      </SimpleGrid>
 
-      {/* TODO MODALVIEWIMAGE */}
+      <ModalViewImage
+        isOpen={isOpen}
+        onClose={onClose}
+        imgUrl={selectedImageURL}
+      />
     </>
   );
 }
